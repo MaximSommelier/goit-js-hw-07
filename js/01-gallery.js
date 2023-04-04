647ecb9e74f3671335cb1a8680986db726abed32
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-04 23:01:58 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// const refs = galleryItems
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({preview,original,description}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`);
+
+gallery.insertAdjacentHTML('beforeend',markup.join(''));
+
+gallery.addEventListener('click', onClick);
+
+function onClick(evt){
+    evt.preventDefault();
+    if (!evt.target.classList.containes('.gallery__image')){
+        return;
+    }
+};
 
 console.log(galleryItems);
